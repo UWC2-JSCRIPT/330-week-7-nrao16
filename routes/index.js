7340892ed110890ca206267986c4577373f12644
Nandini Rao [nandinirao] <nandinirao@costco.com>
2023-05-23 18:50:06 -0700

    initial part of assignment 7

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
 const router = Router();
-
+router.use('/landing', require('./landing'))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const routes = require("./routes");
 
 const server = express();
 server.use(express.json());
+server.use(express.static('static'))
+const mustacheExpress = require('mustache-express');
+server.engine('mustache', mustacheExpress())
+server.set('view engine', 'mustache')
 
 server.use(routes);
 
